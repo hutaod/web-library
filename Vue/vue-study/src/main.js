52f5bb2b80488966e162ca4164ae9bb627326cab
hutao <hutao@hudeMacBook-Pro.lan>
2019-06-27 07:29:37 +0800

    Update main.js

--- Vue/vue-study/src/main.js
+++ Vue/vue-study/src/main.js
@@ -5,17 +5,29 @@ Vue.config.productionTip = false;
 
 Vue.prototype.$dispatch = function(eventName, data) {
   let parent = this.$parent;
+  // 查找父元素
   while (parent) {
-    if (parent) {
-      // 父元素用$emit触发
-      parent.$emit(eventName, data);
-      // 递归查找父元素
-      parent = parent.$parent;
-    } else {
-      break;
-    }
+    // 父元素用$emit触发
+    parent.$emit(eventName, data);
+    // 递归查找父元素
+    parent = parent.$parent;
   }
 };
+
+Vue.prototype.$boardcast = function(eventName, data) {
+  boardcast.call(this, eventName, data);
+};
+
+function boardcast(eventName, data) {
+  this.$children.forEach(child => {
+    // 子元素触发$emit
+    child.$emit(eventName, data);
+    if (child.$children.length) {
+      // 递归调用，通过call修改this指向 child
+      boardcast.call(child, eventName, data);
+    }
+  });
+}
 
 new Vue({
   render: h => h(App)
