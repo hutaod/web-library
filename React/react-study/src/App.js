d88ec7c795a81f5912df57660df930f2628a6a47
xingyang@123 <1131589588@qq.com>
2019-11-22 07:50:22 +0800

    11

--- React/react-study/src/App.js
+++ React/react-study/src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
-import Counter from "./Counter";
+import React from 'react'
+import Counter from './Counter'
+
+const TestComp = (props) => {
+  const { children, color } = props
+  console.log(props)
+
+  return (
+    <button style={{ color }}>
+      {/* <div aria-hiddle={true}>123</div> */}
+      {children}
+    </button>
+  )
+}
 
 function App() {
+  const but = <TestComp add-aa={123}>hello</TestComp>
+  console.log(but.props.children)
+
   return (
     <div className="App">
       <Counter />
+      {but}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
