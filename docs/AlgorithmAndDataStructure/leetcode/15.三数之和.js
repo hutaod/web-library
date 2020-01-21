8f6ca9568aa94d0b809b2bc5b2bde6c5b8f88a1f
hutao1 <hutao@akulaku.com>
2020-01-21 18:24:21 +0800

    feat: 刷题

--- docs/AlgorithmAndDataStructure/leetcode/15.三数之和.js
+++ docs/AlgorithmAndDataStructure/leetcode/15.三数之和.js
@@ -10,27 +10,125 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-  const result = []
-  var obj = {}
+  const res = []
   const cache = {}
-  for (let i = 0; i < nums.length - 2; i++) {
-    rest = 0 - nums[i] // 剩余2数之和
-    for (var j = i + 1; j < nums.length - 1; j++) {
-      // 另一个值
-      var otherIndex = obj[rest - nums[j]]
-      // 另一个值存在于obj中，就返回剩余另一个值的索引
-      if (otherIndex !== undefined) {
-        let arr = [nums[i], nums[j], nums[otherIndex]].sort((a, b) => a - b)
-        let key = arr.join('-')
-        if (!cache[key]) {
-          cache[arr.join('-')] = arr
-          result.push(arr)
+  // 排队，最左边是最小的，最右边是最大的
+  const arr = nums.concat([]).sort((a, b) => a - b)
+
+  // 排序后，如果最大值大于0切最小值小于0才会有解
+  if (arr[0] <= 0 && arr[arr.length - 1] >= 0) {
+    if (arr.length >= 3 && arr[0] === 0 && arr[arr.length - 1] === 0) {
+      return [[0, 0, 0]]
+    }
+    for (let i = 1; i < arr.length - 1; i++) {
+      // c 位人选
+      let first = 0
+      let last = arr.length - 1
+      if (arr[first] > 0 || arr[last] <= 0) {
+        break
+      }
+      while (first < last) {
+        let result = arr[i] + arr[first] + arr[last]
+        if (result === 0 && i !== first && first !== last && i !== last) {
+          // 我们是不是可以一起组队
+          const A = [arr[i], arr[first], arr[last]]
+          const key = A.concat([])
+            .sort((a, b) => a - b)
+            .join('@')
+          if (!cache[key]) {
+            cache[key] = A
+            res.push(A)
+          }
+        }
+        if (result <= 0 && first < i) {
+          // 实力太弱，把菜鸟右移一位
+          ++first // 如果相等就跳过
+        } else if (result > 0 && last > i) {
+          // 实力太强，把大神左移一位
+          --last // 如果相等就跳过
+        } else {
+          break // 某一边已经没有人选了
         }
       }
-      // 另一个值不存在于obj中，就把值作为key，索引作为值存入obj
-      obj[nums[j]] = j
     }
   }
-  return result
+  console.log(res)
+
+  return res
 }
 // @lc code=end
+
+threeSum([0, 0, 0, 0])
+
+/**
+ * 暴力破解法 O(n3)
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+// var threeSum = function(nums) {
+//   const res = []
+//   const hash = {}
+//   const cache = {}
+//   // const arr = nums.concat([]).sort((a, b) => a - b)
+//   const arr = nums.concat([])
+
+//   for (let i = 0; i < arr.length - 2; i++) {
+//     // 每个人
+//     for (let j = i + 1; j < arr.length - 1; j++) {
+//       // 依次拉上其他每个人
+//       for (let k = j + 1; k < arr.length; k++) {
+//         // 去问剩下的每个人
+//         if (arr[i] + arr[j] + arr[k] === 0) {
+//           // 我们是不是可以一起组队
+//           const A = [arr[i], arr[j], arr[k]]
+//           const key = A.concat([])
+//             .sort((a, b) => a - b)
+//             .join('@')
+//           if (!cache[key]) {
+//             cache[key] = A
+//             res.push(A)
+//           }
+//         }
+//       }
+//     }
+//   }
+//   return res
+// }
+
+// @lc code=start
+/**
+ * 暴力破解法 + 第二层缓存 O(n2)
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+// var threeSum = function(nums) {
+//   const res = []
+//   const hash = {}
+//   const cache = {}
+//   const arr = nums.concat([]).sort((a, b) => a - b)
+
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     // 每个人
+//     for (let j = i + 1; j < arr.length - 0; j++) {
+//       let rest = 0 - arr[i] - arr[j]
+//       const hashData = hash[arr[j]]
+//       if (hashData !== undefined && hashData.indexs.every(a => a !== j)) {
+//         // 我们是不是可以一起组队
+//         const A = [...hash[arr[j]].data, arr[j]]
+//         const key = A.concat([])
+//           .sort((a, b) => a - b)
+//           .join('@')
+//         if (!cache[key]) {
+//           cache[key] = A
+//           res.push(A)
+//         }
+//       } else {
+//         hash[rest] = {
+//           data: [arr[i], arr[j]],
+//           indexs: [i, j]
+//         }
+//       }
+//     }
+//   }
+//   return res
+// }
--- docs/AlgorithmAndDataStructure/leetcode/509.斐波那契数.js
+++ docs/AlgorithmAndDataStructure/leetcode/509.斐波那契数.js
@@ -10,6 +10,28 @@
  * @return {number}
  */
 var fib = function(N) {
+  // 递归方式 暴力解法
+  // if (N < 2) {
+  //   return N
+  // }
+  // return fib(N - 1) + fib(N - 2)
+
+  // 递归方式 利用缓存优化
+  // const cache = {}
+  // function fibFn(N) {
+  //   if (N < 2) {
+  //     return N
+  //   }
+  //   // 利用缓存可以有效的减少执行栈
+  //   let prev1 = cache[N - 1] || fibFn(N - 1)
+  //   let prev2 = cache[N - 2] || fibFn(N - 2)
+  //   let res = prev1 + prev2
+  //   cache[N] = res
+  //   return res
+  // }
+  // return fibFn(N)
+
+  // 递推方式 O(n)
   const fibArr = [0, 1]
   for (let i = 2; i <= N; i++) {
     fibArr[i] = fibArr[i - 2] + fibArr[i - 1]
