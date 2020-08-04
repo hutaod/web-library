359e61da7a8b3ea6ae149b6200c519f75fdcbff6
hutao <1131589588@qq.com>
2020-08-04 23:10:13 +0800

    Update 1.两数之和.js

--- docs/AlgorithmAndDataStructure/leetcode/1.两数之和.js
+++ docs/AlgorithmAndDataStructure/leetcode/1.两数之和.js
@@ -10,14 +10,14 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-  var obj = {}
-  for (var i = 0; i < nums.length; i++) {
+const twoSum = function(nums, target) {
+  const obj = {}
+  for (let i = 0; i < nums.length; i++) {
     // 另一个值
-    var otherIndex = obj[target - nums[i]]
+    const otherIndex = obj[target - nums[i]]
     // 另一个值存在于obj中，就返回剩余另一个值的索引
-    if (otherIndex !== undefined) {
-      return [i, otherIndex].sort((a, b) => a - b)
+    if(otherIndex !== undefined) {
+       return [otherIndex, i]
     }
     // 另一个值不存在于obj中，就把值作为key，索引作为值存入obj
     obj[nums[i]] = i
