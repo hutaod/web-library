8da578bb6a2120b443fb1d96e3d7a26d7843deb1
hutao <1131589588@qq.com>
2020-11-29 23:09:20 +0800

    webpack test

---
+++ Demo/demo-react-comp/dist/main.js
@@ -0,0 +1,17 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+eval("function render() {\n  const div = document.createElement('div') \n  div.innerHTML = 'Hello World0'; \n  document.body.appendChild(div) \n}\n\nrender()\n\n//# sourceURL=webpack://demo-react-comp/./src/index.js?");
+/******/ })()
+;
--- Demo/demo-react-comp/webpack.config.basic.js
+++ Demo/demo-react-comp/webpack.config.basic.js
@@ -1,3 +1,8 @@
+const webpack = require("webpack")
+
+/**
+ * @type {webpack.Configuration}
+ */
 module.exports = {
   entry: "./src/index.js",
   mode: "development"
