714d476cea3b742da8d9e41b7661e1dc559bddac
hutao <1131589588@qq.com>
2020-12-01 22:57:41 +0800

    react lib for antd desigin

---
+++ Program/react-lib-design/typings/custom-typings.d.ts
@@ -0,0 +1,37 @@
+// https://github.com/facebook/create-react-app/blob/f09d3d3a52c1b938cecc977c2bbc0942ea0a7e70/packages/react-scripts/lib/react-app.d.ts#L42-L49
+declare module '*.svg' {
+  import * as React from 'react';
+
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+
+  const src: string;
+  export default src;
+}
+
+declare module 'bisheng/collect';
+
+declare module 'bisheng/router';
+
+declare module 'react-github-button';
+
+declare module 'jsonml.js/*';
+
+declare module 'omit.js';
+
+declare module 'rc-animate*';
+
+declare module 'rc-util*';
+
+declare module '@ant-design/css-animation*';
+
+declare module 'rc-radio';
+
+declare module 'rc-editor-mention';
+
+declare module 'rc-queue-anim';
+
+declare module '*.json' {
+  const value: any;
+  export const version: string;
+  export default value;
+}
