bd31bea920b7e0dc68a23199f208cd011bc47dcf
hutao1 <hutao@akulaku.com>
2020-12-04 18:57:33 +0800

    feat: nodejs goole api test

---
+++ Demo/demo-google-sheets-api/test2.js
@@ -0,0 +1,21 @@
+const { google } = require("googleapis");
+
+// Each API may support multiple version. With this sample, we're getting
+// v3 of the blogger API, and using an API key to authenticate.
+const blogger = google.blogger({
+  version: "v3",
+  auth: "YOUR API KEY",
+});
+
+const params = {
+  blogId: "3213900",
+};
+
+// get the blog details
+blogger.blogs.get(params, (err, res) => {
+  if (err) {
+    console.error(err);
+    throw err;
+  }
+  console.log(`The blog url is ${res.data.url}`);
+});
