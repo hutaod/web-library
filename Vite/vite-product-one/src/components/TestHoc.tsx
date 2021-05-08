b4e96599fdb7778b693d082c2d3dea41ad56a5ad
hutao1 <hutao1@akulaku.com>
2021-05-08 19:07:53 +0800

    vite test

---
+++ Vite/vite-product-one/src/components/TestHoc.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./TestHoc.module.css";
+
+function TestHoc() {
+  return <div className={classes.hello}>TestHoc12</div>;
+}
+
+export default TestHoc;
